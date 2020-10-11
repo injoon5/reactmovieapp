e1aafeaad632f7b1f4c572db948423da0f0a3f8e
injoon <injoon5@naver.com>
2020-10-11 16:10:03 +0900

    First ONE!!!

---
+++ precache-manifest.572c0a87bdf3b806a7733db51dcab703.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c2b86620e78a1eaf940fa0a2ead96b",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "a2ed93a26935979eb47f",
+    "url": "/movie_app_2019/static/css/main.672a5149.chunk.css"
+  },
+  {
+    "revision": "2baf1e1f5ab767ca97cc",
+    "url": "/movie_app_2019/static/js/2.3033b0ce.chunk.js"
+  },
+  {
+    "revision": "a2ed93a26935979eb47f",
+    "url": "/movie_app_2019/static/js/main.6c15bdde.chunk.js"
+  },
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  }
+]);
